d0adf1c0fa7e53a5363c61418b404e1f70511e66
FangFeiyue <18766131540@163.com>
2018-09-29 13:41:43 +0800

    组件事件与事件处理

--- project/components/like/index.js
+++ project/components/like/index.js
@@ -4,10 +4,10 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+ 
   },
 
-  /**
+  /** 
    * 组件的初始数据
    */
   data: {
@@ -18,6 +18,8 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onLike(event) {
+      console.log(event);
+    }
   }
 })
