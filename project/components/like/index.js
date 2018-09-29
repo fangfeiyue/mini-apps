ae4671fe61c162612d35a47da94e78df4877cace
FangFeiyue <18766131540@163.com>
2018-09-29 14:43:59 +0800

    let、var与组件事件应用

--- project/components/like/index.js
+++ project/components/like/index.js
@@ -4,14 +4,28 @@ Component({
    * 组件的属性列表
    */
   properties: {
- 
+    like: {
+      // 必填
+      type: Boolean,
+      // 选填
+      value: false,
+      // 选填
+      observer() {
+
+      }
+    },
+    count: {
+      type: Number,
+      value: 0
+    }
   },
 
   /** 
    * 组件的初始数据
    */
   data: {
-
+    yesSrc: 'images/like.png',
+    noSrc: 'images/like@dis.png'
   },
 
   /**
@@ -19,7 +33,13 @@ Component({
    */
   methods: {
     onLike(event) {
-      console.log(event);
+      let { like, count } = this.properties;
+      // 当前的状态是喜欢，点击一下置为不喜欢，所以count要减1
+      count = like ? --count : ++count;
+      this.setData({
+        count,
+        like: !like
+      });
     }
   }
 })
