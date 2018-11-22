1f4786f3b0b38d41b22d14afe4ac94724b6bbde2
FangFeiyue <18766131540@163.com>
2018-11-22 17:40:24 +0800

    音乐播放功能的实现

--- project/components/classic/music/index.js
+++ project/components/classic/music/index.js
@@ -2,28 +2,35 @@
 
 import { classicBeh } from '../classic-beh';
 
+const mMgr = wx.getBackgroundAudioManager();
+
 Component({
   /**
    * Component properties
    */
   behaviors: [classicBeh],
   properties: {
-    img: String,
-    content: String
+    src: String
   },
 
   /**
    * Component initial data
    */
   data: {
-    pauseSrc: './images/player@waitting.png',
-    playSrc: './images/player@playing.png'
+    isPlaying: false,
+    pauseSrc: './images/player@playing.png',
+    playSrc: './images/player@waitting.png'
   },
 
   /**
    * Component methods
    */
   methods: {
-
+    onPlay(event){
+      this.setData({
+        isPlaying: !this.data.isPlaying
+      });
+      this.data.isPlaying ? mMgr.src = this.properties.src : mMgr.pause();
+    }
   }
 })
