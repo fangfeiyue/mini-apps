1960ce45820c7ac08f96be4256b59659db2ca2d3
FangFeiyue <18766131540@163.com>
2018-11-27 14:23:59 +0800

    点击音乐总开关关闭音乐后音乐图片还是显示的播放状态

--- project/components/classic/music/index.js
+++ project/components/classic/music/index.js
@@ -21,6 +21,15 @@ Component({
     pauseSrc: './images/player@playing.png',
     playSrc: './images/player@waitting.png'
   },
+ 
+  attached(){
+    this._recoverStatus();
+    this._monitorSwitch();
+  },
+
+  detached(){
+    // mMgr.stop();
+  },
 
   /**
    * Component methods
@@ -31,6 +40,34 @@ Component({
         isPlaying: !this.data.isPlaying
       });
       this.data.isPlaying ? mMgr.src = this.properties.src : mMgr.pause();
+    },
+    _recoverStatus(){
+      if (mMgr.paused){
+        this.setData({
+          isPlaying: false
+        });
+        return;
+      }
+
+      if (mMgr.src == this.properties.src){
+        this.setData({
+          isPlaying: true
+        });
+      }
+    },
+    _monitorSwitch(){
+      mMgr.onPlay(()=>{
+        this._recoverStatus()  
+      });
+      mMgr.onPause(()=>{
+        this._recoverStatus()
+      });
+      mMgr.onStop(()=>{
+        this._recoverStatus()
+      });
+      mMgr.onEnded(()=>{
+        this._recoverStatus()
+      });
     }
   }
 })
